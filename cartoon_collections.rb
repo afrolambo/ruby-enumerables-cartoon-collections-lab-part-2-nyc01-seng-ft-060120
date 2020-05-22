6ce98a0b81a4cfff938fa3be8bc5ff384e99ca3f
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-22 00:35:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ end
 
 def long_planeteer_calls(planeteer_calls)
   planeteer_calls.all? {
-    |planeteer_calls| planeteer_calls.length
+    |planeteer_calls| planeteer_calls.length > 4
   }
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
