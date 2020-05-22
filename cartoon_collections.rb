1f4f2164bc6e97b4889e09236dd25385ef6f26ed
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-22 00:14:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,8 @@
 def square_array(array)
   new_array = []
-  index = 0 
-  while index < array.length do 
-    new_array << array[index] * array[index] 
-    index += 1 
+ array.each do |element|
+    new_array << element * element
   end 
-  # Return a new array of the results
   new_array
 end
 
