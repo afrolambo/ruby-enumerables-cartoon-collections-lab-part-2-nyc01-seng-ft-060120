cc531e29cc6afb3958d4f53fda090151e038d0eb
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-22 01:14:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.include? do |word|
-   if word = valid_calls
+   if word = valid_calls.any?
      return word 
     else 
       nil 
