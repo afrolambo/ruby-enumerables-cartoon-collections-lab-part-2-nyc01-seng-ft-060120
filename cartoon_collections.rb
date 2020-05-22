98854161cc4c04bcd35b3171ba7cbd579b3c6301
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-22 01:09:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.any? |word| 
-    if word = valid_calls.any?
+    if word = valid_calls
       return word 
   end 
   # Use an Enumerable to check if any elements passed in the array match the valid calls listed above 
