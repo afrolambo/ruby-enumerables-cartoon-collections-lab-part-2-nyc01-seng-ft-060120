1a8dad864497a7f31e63090e5a782d680a19e6a8
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-22 01:42:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,8 +25,6 @@ def find_valid_calls(planeteer_calls)
   if planeteer_calls.each do |word|
     word.any?(valid_calls)
     return word 
-  else
-    nil 
   end 
   
   # Use an Enumerable to check if any elements passed in the array match the valid calls listed above 
